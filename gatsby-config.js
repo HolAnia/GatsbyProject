3d368e956c73753d04b0c1a41cf60ffb5ee6a0c8
Ania Hołomiej <40638570+HolAnia@users.noreply.github.com>
2023-04-20 09:14:20 +0200

    set up project

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `CakeIt!`,
+    description: `fun page of ckaes!`,
+    author: `@HolAnia`
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -23,6 +22,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -39,5 +39,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Teko\:200, 400, 500, 600, 700`],
+        display: `swap`,
+      } 
+    }
   ],
 }
