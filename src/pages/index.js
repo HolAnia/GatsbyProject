9704f9fe09398522b5b348630febe744b91f854f
Ania Hołomiej <40638570+HolAnia@users.noreply.github.com>
2023-04-20 10:29:22 +0200

    add data from graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { getImage, GatsbyImage} from 'gatsby-plugin-image';
 
-const IndexPage = () => (
-  <div>Start Here</div>
-);
+const IndexPage = ({ data }) => {
+  console.log(data.fileInformation.edges)
+  return(
+    <>
+      <div>Start Here</div>
+      <ul>
+        {data.fileInformation.edges.map(({ node }) => {
+          return <li key = {node.id}>{ node.base } | { node.prettySize }</li>
+          }
+        )}
+      </ul>
+      <div>
+      {data.fileInformation.edges.map(({ node }) => {
+          return (
+            <img src={node.publicURL}/>
+          )
+          }
+        )}
+      </div>
+    </>
+  )
+};
 
 export default IndexPage;
+
+export const query = graphql`
+  query {
+    fileInformation: allFile {
+      edges {
+        node {
+          id
+          base
+          prettySize
+          childrenImageSharp {
+            gatsbyImageData(width: 600)
+          }
+          publicURL
+        }
+      }
+    }
+  }
+`;
